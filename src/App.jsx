1021082c32654258cde52e4cefd377929b5cd772
sandraovjones-sudo <sandra.ov.jones@gmail.com>
2025-09-25 08:56:49 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import CustomerLifecycleTimeline from "./components/CustomerLifecycleTimeline";
 export default function App() {
   const [query, setQuery] = useState("");
